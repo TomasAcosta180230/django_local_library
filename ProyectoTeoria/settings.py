0357ea04ead8a535db18de69e4da1ad53d7a4a74
Tomas Acosta <al180230@alumnos.uacj.mx>
2020-10-13 16:52:03 -0600

    Update the host allowed

--- ProyectoTeoria/settings.py
+++ ProyectoTeoria/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '#4)unt^rfh%oa-i(^4l%cpmv(t(r_9#9o1vb!x!pu#!i4*lz3#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://quiet-lake-90416.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
